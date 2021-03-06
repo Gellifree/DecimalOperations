5065106a6b04b6ff54983d9a77caab66f172408f
Gellifree <forge.deladortitan@gmail.com>
2021-03-06 20:02:32 +0100

    bármilyen mélységű zárójelezés megvalósítása, minden elem egyenlőre szövegként van kezelve, nincs átkonvertálva számmá.

--- Source/main.py
+++ Source/main.py
@@ -14,139 +14,44 @@ import menu, operators, syntaxChecker
 op = operators.Operators()
 sc = syntaxChecker.SyntaxChecker()
 
-
 def slice(string):
-    result = []
-    number = ""
-    direction = "in"
 
-    isItInParent = False
-    #print("Kapott felvágandó szöveg: ", string)
+    result = []
+
+    collector = ""
+    operator = ""
+
+    parentDeepness = 0 #Out
+
     for char in string:
         if(char == "("):
-            isItInParent = True
-            direction = "in"
-
-
+            parentDeepness += 1
         if(char == ")"):
-            direction = "out"
-            isItInParent = False
-        if(isItInParent == False):
-            if(direction == "out"):
-                number += char
-                result.append(number.replace(" ", ""))
-                direction = "undefined"
-                number = ""
-
-            if(char != ")"):
-                if(sc.isItOperator(char) == True):
-                    if(number.strip() == "" and direction != "undefined"):
-                        result.append(0)
-                    else:
-                        if(number.strip() != ""):
-                            result.append(float(number.replace(" ", "")))
-                    number = ""
-                    result.append(char)
-                else:
-                    number += char
-        else:
-            number += char
+            parentDeepness -= 1
 
 
+        if(sc.isItOperator(char) == False and parentDeepness == 0):
+            collector += char
+            if(char == string[-1]):
+                result.append(collector)
+        elif(sc.isItOperator(char) == True and parentDeepness == 0):
+            result.append(collector)
+            collector = ""
+            operator = char
+            result.append(operator)
+        elif(parentDeepness > -1):
+            collector += char
 
-    if(number.strip() == "" and direction != "undefined"):
-        result.append(0)
-    else:
-        if(number.strip() != ""):
-            result.append(float(number.replace(" ", "")))
-    return result
+    print(result)
 
-
-
-def connect(a, b, operator):
-    if(operator == "+"):
-        return op.add(a,b)
-    if(operator == "-"):
-        return op.subtract(a,b)
-    if(operator == "*"):
-        return op.times(a,b)
-    if(operator == "/"):
-        return op.divine(a,b)
-
-def maximumSearch(list):
-    max = -10
-    index = 0
-    for i in range(len(list)):
-        if(list[i] > max):
-            max = list[i]
-            index = i
-
-    return index
-
-
-def primaryOperation(taskList):
-    result = []
-    for i in range(len(taskList)):
-        if(sc.isItOperator(taskList[i]) == True):
-            if(taskList[i] == "+" or taskList[i] == "-"):
-                result.append(1)
-            elif(taskList[i] == "*" or taskList[i] == "/"):
-                result.append(2)
-        else:
-            result.append(-1)
-    return maximumSearch(result)
-
-def dissolveParent(list):
-    print("Szeletelt lista: ", list)
-    inParent = ""
-    index = 0
-    for element in list:
-        if type(element) is str:
-            if(element[0] == "("):
-                for i in range(1, len(element)-1):
-                    inParent += element[i]
-                #print("inParent: ", inParent)
-                inParent = slice(inParent)
-                result = calculate(inParent)
-
-                #print(result)
-
-                #print("korai lista: ", list)
-                del list[index]
-
-                list.insert(index, result)
-                #print("Új lista: ", list)
-                inParent = ""
-        index += 1
-    return list
-
-
-def calculate(tasks):
-    print("A calculate kapta: ", tasks)
-    sTasks = tasks
-    taskList = []
-    taskList = tasks
-    #print(taskList)
-    while(len(sTasks) > 1):
-        #print("Feladatok: ",taskList)
-        operationDone = primaryOperation(taskList)
-        calculationResult = connect(taskList[operationDone -1], taskList[operationDone + 1], taskList[operationDone])
-        #print("Részeredmény: ", calculationResult)
-        del taskList[operationDone-1:operationDone+2]
-        taskList.insert(operationDone-1, calculationResult)
-        sTasks = taskList
-    return calculationResult
 
 
 def main():
-    task = "(3 + 3) -2 * (3 * 1) - 1 * (3 / 3) - (9 - 4) - (3 * 3 * 2)"
-    #task = "2 * 2 + 2 - 8"
-    #task = input("Add meg az elvégezendő műveletet: ")
-    #calculate(task)
-
-    result = dissolveParent(slice(task))
-    print("Felbontott:\t ", result)
-    print("Eredmény:\t ", calculate(result))
+    tasks = " 3 + 1 -  4 /     9"
+    tasks = "3 - (1 + (3 + (1 -1) + 3)) - 4 + 88.3 - 2 + (3 + 3 -1) - 3"
+    tasks = tasks.replace(" ", "")
+    print(tasks)
+    slice(tasks)
 
 
 if __name__ == '__main__':
